ca555cbc5d688eadd23f456afb3a3b1f2106132f
Amanda Reahana Gomes <67775406+Reahana@users.noreply.github.com>
2021-12-13 20:17:29 +0600

    class 37

---
+++ class_37/pages/dashboard.php
@@ -0,0 +1,17 @@
+<?php include  './includes/header.php' ?>
+
+
+<section class="py-5 text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <div class="card card-body">
+                  <h3 class="text-center">My Dashboard</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include  './includes/footer.php' ?>
