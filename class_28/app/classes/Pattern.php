b9d3483f483949774ed760341e288e0ebed911d2
Amanda Reahana Gomes <67775406+Reahana@users.noreply.github.com>
2021-12-14 10:15:46 +0600

    class 28

---
+++ class_28/app/classes/Pattern.php
@@ -0,0 +1,112 @@
+<?php
+namespace App\classes;
+
+class Pattern 
+{
+   public $n=5;
+   public $i;
+   public $j;
+
+    public function patternOne()
+    {
+        
+
+         for($this->i=1;$this->i<=$this->n;$this->i++)
+        {
+            for ($this->j=1 ; $this->j<=($this->n*2)-1 ; $this->j++) 
+            { 
+                if (($this->j>= $this->n-($this->i-1))&& ($this->j<= $this->n+($this->i-1)))
+                    {
+                    echo '*';
+                    }
+                else
+                {
+                    echo '&nbsp;&nbsp;';
+                }
+                
+            }
+            echo '<br/>';
+        }
+      
+      
+       
+        
+    }
+    public function patternTwo()
+    {
+        for($this->i=1;$this->i<=$this->n;$this->i++)
+        {
+            for ($this->j=1 ; $this->j<=($this->n*2)-1 ; $this->j++) 
+            { 
+                if (($this->j>= $this->n-($this->i-1))&& ($this->j<= $this->n+($this->i-1)))
+                    {
+                        echo '*';
+                    }
+                else
+                {
+                    echo '&nbsp;&nbsp;';
+                }
+                
+            }
+            echo '<br/>';
+        }
+      
+      
+        for($this->i=$this->n-1;$this->i>=1;$this->i--)
+        {
+            for ($this->j=1 ; $this->j<=($this->n*2)-1 ; $this->j++) 
+            { 
+                if (($this->j>= $this->n-($this->i-1))&& ($this->j<= $this->n+($this->i-1)))
+                    {
+                    echo '*';
+                    }
+                else
+                {
+                    echo '&nbsp;&nbsp;';
+                }
+                
+            }
+            echo '<br/>';
+        }
+    }
+    public function patternThree()
+    {
+        for ($this->i=1; $this->i <=$this->n ; $this->i++) { 
+            for ($this->j=1 ; $this->j<=$this->n ; $this->j++) 
+            {
+                if ($this->i==1 || $this->i==$this->n) 
+                {
+                    echo '*';
+                }
+                else
+                {
+                    if ($this->j==1 || $this->j==$this->n) 
+                    {
+                        echo '*';
+                    }
+                    else{
+                        echo '&nbsp;&nbsp;';
+                    }
+                }
+                
+            }
+            echo'<br/>';
+        }
+    }
+    public function patternFour()
+    {
+        for ($this->i=0; $this->i <= $this->n ; $this->i++) 
+        { 
+            for ($this->j=1 ; $this->j<=$this->n ; $this->j++) 
+            {
+                if ($this->j==$this->i) {
+                    echo '*';
+                }
+                else{
+                    echo '&nbsp;&nbsp;';
+                }
+            }
+            echo '<br/>';
+        }
+    }
+}
---
+++ class_28/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use App\classes\Pattern;
+
+$pattern= new Pattern();
+// $pattern->patternOne();
+// $pattern->patternTwo();
+// $pattern->patternThree();
+$pattern->patternFour();
